acc90f27afa9fab9e084e21977d71905b4784244
hilk <ilkhayriye@gmail.com>
2023-01-31 03:57:42 -0800

    gitignore updated, ThreeSum's bruteforce solution added

---
+++ leetcode-solutions/leetcode-solutions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetcode_solutions
+{
+    class Program
+    {
+        static void Main()
+        {
+            var threeSum = new ThreeSum();
+            var result = threeSum.Solution(new int[] { -1, 0, 1, 2, -1, -4 });
+            result = threeSum.Solution(new int[] { 0, 1, 1 });
+            result = threeSum.Solution(new int[] { 0, 0, 0 });
+        }
+    }
+}
---
+++ leetcode-solutions/leetcode-solutions/ThreeSum.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_solutions
+{
+    public class ThreeSum
+    {
+        //Brute force solution
+        public IList<IList<int>> Solution(int[] arr)
+        {
+            var len = arr.Length;
+            var pairs = new List<Pair>();
+
+            for (int i = 0; i < len; i++)
+            {
+                for (int j = i; j < len; j++)
+                {
+                    for (int k = j; k < len; k++)
+                    {
+                        if (i == k || i == j || j == k)
+                        {
+                            continue;
+                        }
+
+                        if (arr[i] + arr[j] + arr[k] == 0)
+                        {
+                            var pair = new Pair(arr[i], arr[j], arr[k]);
+                            if (!pairs.Any(x => x.Equals(pair)))
+                            {
+                                pairs.Add(pair);
+                            }
+                        }
+                    }
+                }
+            }
+
+            var result = new List<IList<int>>();
+            foreach (var pair in pairs)
+            {
+                result.Add(pair.ToList());
+            }
+
+            return result;
+        }
+    }
+
+    public class Pair : IEquatable<Pair>
+    {
+        public int Element0 { get; private set; }
+        public int Element1 { get; private set; }
+        public int Element2 { get; private set; }
+
+        public Pair(int element0, int element1, int element2)
+        {
+            Element0 = element0;
+            Element1 = element1;
+            Element2 = element2;
+        }
+
+        public List<int> ToList()
+        {
+            return new List<int> { Element0, Element1, Element2 };
+        }
+
+        public bool Equals(Pair? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if ((other.Element0 == Element0 ||
+                 other.Element0 == Element1 ||
+                 other.Element0 == Element2) &&
+                 (other.Element1 == Element0 ||
+                 other.Element1 == Element1 ||
+                 other.Element1 == Element2) &&
+                 other.Element2 == Element0 ||
+                 other.Element2 == Element1 ||
+                 other.Element2 == Element2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null ||
+                obj.GetType() != typeof(Pair))
+                return false;
+
+            return Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (int)(Math.Pow(2, Element0) + Math.Pow(2, Element1) + Math.Pow(2, Element2));
+        }
+    }
+}
