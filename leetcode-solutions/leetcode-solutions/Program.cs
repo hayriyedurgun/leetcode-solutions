9eebfea0d50aaa2e1323b11e0bdc0a59616242dd
hilk <ilkhayriye@gmail.com>
2023-02-01 03:49:28 -0800

    SetMatrixZeros added

--- leetcode-solutions/leetcode-solutions/Program.cs
+++ leetcode-solutions/leetcode-solutions/Program.cs
@@ -9,10 +9,18 @@ namespace leetcode_solutions
     {
         static void Main()
         {
-            var threeSum = new ThreeSum();
-            var result = threeSum.Solution(new int[] { -1, 0, 1, 2, -1, -4 });
-            result = threeSum.Solution(new int[] { 0, 1, 1 });
-            result = threeSum.Solution(new int[] { 0, 0, 0 });
+            //var matrixZeroes = new SetMatrixZeros();
+            //var arr = new int[][] {
+            //    new int[]{-4,-2147483648,6,-7,0},
+            //    new int[]{-8,6,-8,-6,0},
+            //    new int[]{ 2147483647, 2, -9, -6, -10 }
+            //};
+            //matrixZeroes.SetZeroes(arr);
+
+            var sum = new TwoSum();
+            var result = sum.Solution(new int[] { 2, 7, 11, 15 }, 9);
+            result = sum.Solution(new int[] { 3, 2, 4 }, 6);
+            result = sum.Solution(new int[] { 3, 3 }, 6);
         }
     }
 }
---
+++ leetcode-solutions/leetcode-solutions/SetMatrixZeros.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode_solutions
+{
+    internal class SetMatrixZeros
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            var rowIndexes = Enumerable.Range(0, matrix.Length).ToList();
+            var columnIndexes = Enumerable.Range(0, matrix[0].Length).ToList();
+
+
+            SetZeroesInternal(matrix, 0, 0, rowIndexes, columnIndexes);
+
+            for (int i = 0; i <matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (!rowIndexes.Contains(i) || !columnIndexes.Contains(j))
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+        }
+
+        private void SetZeroesInternal(int[][] matrix, int row, int col, List<int> rowIndexes, List<int> columnIndexes)
+        {
+            for (var j = col; j < matrix[0].Length; j++)
+            {
+                var item = matrix[row][j];
+
+                if (item == 0)
+                {
+                    rowIndexes.Remove(row);
+                    columnIndexes.Remove(j);
+                }
+            }
+
+            for (var i = row; i < matrix.Length; i++)
+            {
+                var item = matrix[i][col];
+
+                if (item == 0)
+                {
+                    rowIndexes.Remove(i);
+                    columnIndexes.Remove(col);
+                }
+            }
+
+            row++;
+            col++;
+            if (row < matrix.Length && col < matrix[0].Length)
+            {
+                SetZeroesInternal(matrix, row, col, rowIndexes, columnIndexes);
+            }
+        }
+    }
+}
